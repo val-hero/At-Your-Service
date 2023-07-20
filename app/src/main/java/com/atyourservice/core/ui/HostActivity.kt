bc9fbd5eb38b766c97f7628b6c8d7ce1d9e6405a
valhero <valhero.lmp@gmail.com>
2023-07-20 23:37:51 +0300

    refactor: подправил вёрстку на более "резиновую", добавил вводимые типы для edit text, добавил скрытие bottomNav, удалил заглушку BlankFragment

--- app/src/main/java/com/atyourservice/core/ui/HostActivity.kt
+++ app/src/main/java/com/atyourservice/core/ui/HostActivity.kt
@@ -2,11 +2,26 @@ package com.atyourservice.core.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
 import com.example.atyourservice.R
+import com.example.atyourservice.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_host)
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHost.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.isVisible = when(destination.id) {
+                R.id.authorizationFragment -> false
+                else -> true
+            }
+        }
     }
 }
