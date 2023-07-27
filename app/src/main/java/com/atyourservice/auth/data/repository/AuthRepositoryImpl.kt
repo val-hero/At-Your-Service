2d7cdceed2902200a520ca2ee7083d57c7aef19d
valhero <valhero.lmp@gmail.com>
2023-07-27 16:32:06 +0300

    feat: выполнение функций репозитория перенесено в IO поток

--- app/src/main/java/com/atyourservice/auth/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/atyourservice/auth/data/repository/AuthRepositoryImpl.kt
@@ -4,27 +4,33 @@ import com.atyourservice.auth.domain.repository.AuthRepository
 import com.atyourservice.core.utils.ErrorType
 import com.atyourservice.core.utils.TaskResult
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class AuthRepositoryImpl(private val authClient: FirebaseAuth) : AuthRepository {
 
     override suspend fun signUp(email: String, password: String): TaskResult<Boolean> {
-        if(authClient.currentUser != null) return TaskResult.Error(ErrorType.AlreadySignedIn)
+        if (authClient.currentUser != null) return TaskResult.Error(ErrorType.AlreadySignedIn)
 
         return try {
-            authClient.createUserWithEmailAndPassword(email, password).await()
-            TaskResult.Success(true)
+            withContext(Dispatchers.IO) {
+                authClient.createUserWithEmailAndPassword(email, password).await()
+                TaskResult.Success(true)
+            }
         } catch (e: Exception) {
             TaskResult.Error(getAuthError(e.message))
         }
     }
 
     override suspend fun signIn(email: String, password: String): TaskResult<Boolean> {
-        if(authClient.currentUser != null) return TaskResult.Error(ErrorType.AlreadySignedIn)
+        if (authClient.currentUser != null) return TaskResult.Error(ErrorType.AlreadySignedIn)
 
         return try {
-            authClient.signInWithEmailAndPassword(email, password).await()
-            TaskResult.Success(true)
+            withContext(Dispatchers.IO) {
+                authClient.signInWithEmailAndPassword(email, password).await()
+                TaskResult.Success(true)
+            }
         } catch (e: Exception) {
             TaskResult.Error(getAuthError(e.message))
         }
@@ -32,8 +38,10 @@ class AuthRepositoryImpl(private val authClient: FirebaseAuth) : AuthRepository 
 
     override suspend fun sendNewPassword(email: String): TaskResult<Boolean> {
         return try {
-            authClient.sendPasswordResetEmail(email).await()
-            TaskResult.Success(true)
+            withContext(Dispatchers.IO) {
+                authClient.sendPasswordResetEmail(email).await()
+                TaskResult.Success(true)
+            }
         } catch (e: Exception) {
             TaskResult.Error(getAuthError(e.message))
         }
